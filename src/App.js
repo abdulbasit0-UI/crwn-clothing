6dd96464df939462b04902f06b741df0c278743d
Abdulbasit <habdulbasit099@gmail.com>
2022-02-02 09:23:33 +0200

    setup navigation with react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/Homepage.components";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hello Hats</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
